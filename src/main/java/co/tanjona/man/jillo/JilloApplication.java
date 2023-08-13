7be8a8ba65de6a96ce7761ea2d946db375e3aa6d
tanjonaaa <hei.tanjona@gmail.com>
2023-08-13 11:41:17 +0300

    First commit

---
+++ src/main/java/co/tanjona/man/jillo/JilloApplication.java
@@ -0,0 +1,13 @@
+package co.tanjona.man.jillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JilloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JilloApplication.class, args);
+    }
+
+}
