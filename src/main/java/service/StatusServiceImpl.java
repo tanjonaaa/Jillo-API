bed0df9ad6fbf1eb1cf59c3898e7ed21c8c4dc36
tanjonaaa <hei.tanjona@gmail.com>
2023-08-17 18:50:31 +0300

    feat: finished Status Service (getAll, getById, add, update, delete)

--- src/main/java/service/StatusServiceImpl.java
+++ src/main/java/service/StatusServiceImpl.java
@@ -3,11 +3,9 @@ package service;
 import model.Status;
 import org.springframework.stereotype.Service;
 import repository.StatusRepository;
-
 import java.util.List;
 @Service
 public class StatusServiceImpl implements StatusService{
-
     private StatusRepository repository;
 
     public StatusServiceImpl(StatusRepository repository) {
@@ -16,26 +14,45 @@ public class StatusServiceImpl implements StatusService{
 
     @Override
     public List<Status> getAllStatus() {
-        return null;
+        return this.repository.all();
     }
 
     @Override
     public Status getStatusById(int id) {
-        return null;
+        return this.repository.oneById(id);
     }
 
     @Override
     public Status addStatus(Status status) {
-        return null;
+        Status foundStatus = this.repository.oneByName(status.getName());
+        if(foundStatus.getId() != 0){
+            return null;
+        }else {
+            System.out.println("Insertion");
+            this.repository.save(status);
+            return this.repository.oneByName(status.getName());
+        }
     }
 
     @Override
     public Status updateStatus(Status status) {
-        return null;
+        Status foundStatus = this.repository.oneById(status.getId());
+        if(foundStatus.getId() != 0){
+            foundStatus.setName(status.getName());
+            this.repository.update(foundStatus);
+            return this.repository.oneById(status.getId());
+        }else {
+            return null;
+        }
     }
 
     @Override
     public void deleteStatus(int id) {
-
+        Status foundStatus = this.repository.oneById(id);
+        if(foundStatus.getId() != 0){
+            this.repository.delete(foundStatus);
+        }else {
+            System.out.println("Suppression ratée");
+        }
     }
 }
