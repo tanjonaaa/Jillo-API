8436e6fc375935156e8856cadf34939c581d67d4
tanjonaaa <hei.tanjona@gmail.com>
2023-08-29 22:20:53 +0300

    refactor: removed useless interfaces services

--- src/main/java/service/StatusService.java
+++ src/main/java/service/StatusService.java
@@ -1,13 +1,48 @@
 package service;
 
 import model.Status;
+import org.springframework.stereotype.Service;
+import repository.JdbcStatusRepository;
 
 import java.util.List;
-
-public interface StatusService {
-    List<Status> getAllStatus();
-    Status getStatusById(int id);
-    Status addStatus(Status status);
-    Status updateStatus(Status status);
-    int deleteStatus(int id);
+@Service
+public class StatusService {
+    private final JdbcStatusRepository repository;
+    public StatusService(JdbcStatusRepository repository) {
+        this.repository = repository;
+    }
+    public List<Status> getAllStatus() {
+        return this.repository.all();
+    }
+    public Status getStatusById(int id) {
+        return this.repository.oneById(id);
+    }
+    public Status addStatus(Status status) {
+        Status foundStatus = this.repository.oneByUniqueColumn(status.getName());
+        if(foundStatus != null){
+            return null;
+        }else {
+            this.repository.save(status);
+            return this.repository.oneByUniqueColumn(status.getName());
+        }
+    }
+    public Status updateStatus(Status status) {
+        Status foundStatus = this.repository.oneById(status.getId());
+        if(foundStatus != null){
+            foundStatus.setName(status.getName());
+            this.repository.update(foundStatus, foundStatus.getId());
+            return this.repository.oneById(status.getId());
+        }else {
+            return null;
+        }
+    }
+    public int deleteStatus(int id) {
+        Status foundStatus = this.repository.oneById(id);
+        if(foundStatus != null){
+            this.repository.delete(id);
+            return 1;
+        }else {
+            return 0;
+        }
+    }
 }
--- src/main/java/service/TaskService.java
+++ src/main/java/service/TaskService.java
@@ -1,13 +1,80 @@
 package service;
 
+import model.Project;
 import model.Task;
+import model.User;
+import org.springframework.stereotype.Service;
+import repository.JdbcProjectRepository;
+import repository.JdbcTaskRepository;
+import repository.JdbcUserRepository;
 
 import java.util.List;
 
-public interface TaskService {
-    List<Task> getAllTasks();
-    Task getTaskById(int id);
-    Task addTask(Task task);
-    Task updateTask(Task task);
-    int deleteTask(int id);
+@Service
+public class TaskService {
+    private final JdbcTaskRepository repository;
+    private final JdbcProjectRepository projectRepository;
+    private final JdbcUserRepository userRepository;
+    public TaskService(JdbcTaskRepository repository, JdbcProjectRepository projectRepository, JdbcUserRepository userRepository) {
+        this.repository = repository;
+        this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
+    }
+    public List<Task> getAllTasks() {
+        return this.repository.all();
+    }
+    public Task getTaskById(int id) {
+        return this.repository.oneById(id);
+    }
+    public Task addTask(Task task) {
+        Task foundTask = this.repository.oneByUniqueColumn(task.getTitle());
+        if(foundTask != null){
+            return null;
+        }else {
+            //Check if the project exists
+            Project foundProject = this.projectRepository.oneById(task.getIdProject());
+            if(foundProject != null){
+                //Check if the user is collaborator in the project
+                List<Project> userProjects = this.userRepository.getProjects(task.getIdUser());
+                List<Project> filteredProjects = userProjects.stream()
+                        .filter((p) -> p.getId().equals(task.getIdProject())).toList();
+                System.out.println(filteredProjects);
+                if(filteredProjects.size() > 0){
+                    this.repository.save(task);
+                    return this.repository.oneByUniqueColumn(task.getTitle());
+                }else{
+                    return null;
+                }
+            }else {
+                return null; //500
+            }
+        }
+    }
+    public Task updateTask(Task task) {
+        Task foundTask = this.repository.oneById(task.getId());
+        if(foundTask != null){
+            foundTask.setTitle(task.getTitle());
+            foundTask.setDescription(task.getDescription());
+            foundTask.setDeadline(task.getDeadline());
+            foundTask.setIdUser(task.getIdUser());
+            foundTask.setIdProject(task.getIdProject());
+            foundTask.setIdStatus(task.getIdStatus());
+            this.repository.update(foundTask, foundTask.getId());
+            return this.repository.oneById(task.getId());
+        }else {
+            return null;
+        }
+    }
+    public int deleteTask(int id) {
+        Task foundTask = this.repository.oneById(id);
+        if(foundTask != null){
+            this.repository.delete(id);
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+    public List<User> showAssignees(int id){
+        return this.repository.getUsers(id);
+    }
 }
